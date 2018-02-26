bd06bcb4d7721813abdcfeb35d0e08a0fb933ee0
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-02-26 08:12:38 +0100

    [YCMPSIO-0] Initial commit

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -2,12 +2,16 @@ cp -R ../../c++/ workdir
 mkdir -p ../../../../target/classes/linux64/
 cd workdir
 ./configure \
---prefix=$PWD/release 
+--prefix=$PWD/release \
+--privsep-user=$USER
 
+make
 make install
 r1=$?
 cp -R release/lib ../../../../../target/classes/linux64
 cp -R release/include ../../../../../target/classes/linux64
 cd ..
+rm -r workdir
+rm -R alsa
 
 return $r1
