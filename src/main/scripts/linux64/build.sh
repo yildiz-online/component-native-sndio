d4a699cf50fdceff5ee8b31da7195877945a3685
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-03-18 13:04:03 +0100

    Release 1.4.0.0

--- src/main/scripts/linux64/build.sh
+++ src/main/scripts/linux64/build.sh
@@ -1,5 +1,9 @@
 cp -R ../../c++/ workdir
 mkdir -p ../../../../target/classes/linux64/
+
+export C_INCLUDE_PATH=$PWD/alsa/linux64/include
+export LIBRARY_PATH=$PWD/alsa/linux64/lib
+
 cd workdir
 ./configure \
 --prefix=$PWD/release \
@@ -11,7 +15,7 @@ r1=$?
 cp -R release/lib ../../../../../target/classes/linux64
 cp -R release/include ../../../../../target/classes/linux64
 cd ..
+rm -r alsa
 rm -r workdir
-rm -R alsa
 
 return $r1
